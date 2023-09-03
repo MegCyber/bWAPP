1db7c1377144b7027dca68623b4bf190204924a9
MegCyber <m.olowokere@rgu.ac.uk>
2023-09-03 18:03:04 +0100

    To generate reports

--- src/test/java/com/project/TestRunner.java
+++ src/test/java/com/project/TestRunner.java
@@ -8,9 +8,11 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com/project/test"},
-        plugin = {"pretty",
-                "json:target/cucumber-report/cucumber.json",
-                "html:target/cucumber-reports/cucumber.html"},
+        plugin = {"summary", "pretty",
+               "json:target/cucumber-report/cucumber.json",
+                "html:target/cucumber-report/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+                },
         monochrome = true
 
 )
